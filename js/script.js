e5edefc8138ba445c9294770efbe060e57fe0128
Italo souza sampaio <italo12346@gmail.com>
2023-03-13 21:46:43 -0300

    implementação de menu hamburguer responsivo

---
+++ js/script.js
@@ -0,0 +1,19 @@
+var btnMenu = document.querySelector(".btn-menu");
+var menuClick = document.querySelector(".menu");
+let cont = 0
+btnMenu.addEventListener("click", function () {
+    
+    if(cont==0){
+        document.querySelector(".sideBar-active").setAttribute('class', 'sideBar-show')
+        btnMenu.setAttribute('class', 'btn-menuON')
+        cont++
+        console.log(cont);
+    }else{
+        document.querySelector("#sideBar-disable").setAttribute('class', 'sideBar-active')
+        btnMenu.setAttribute('class', 'btn-menu')
+        cont=0
+        console.log(cont);
+    }
+       
+});
+
