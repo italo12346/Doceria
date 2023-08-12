0988d7172e06b17ef63a3f5d1c69ebc9478fff0a
Italo souza sampaio <italo12346@gmail.com>
2023-08-12 19:49:11 -0300

    Implementando carroucel funcional

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 var btnMenu = document.querySelector(".btn-menu");
 var menuClick = document.querySelector(".menu");
 let cont = 0
+
 btnMenu.addEventListener("click", function () {
     
     if(cont==0){
@@ -17,8 +18,40 @@ btnMenu.addEventListener("click", function () {
        
 });
 
-document.querySelector(".btn-submit").addEventListener('click',()=>{
-    window.alert("obrigado pelo contato")
-})
+
 /* CARROUSEL */
 
+
+const controls = document.querySelectorAll(".control");
+let currentItem = 0;
+const items = document.querySelectorAll(".item");
+const maxItems = items.length;
+
+controls.forEach((control) => {
+  control.addEventListener("click", (e) => {
+    isLeft = e.target.classList.contains("arrow-left");
+
+    if (isLeft) {
+      currentItem -= 1;
+    } else {
+      currentItem += 1;
+    }
+
+    if (currentItem >= maxItems) {
+      currentItem = 0;
+    }
+
+    if (currentItem < 0) {
+      currentItem = maxItems - 1;
+    }
+
+    items.forEach((item) => item.classList.remove("current-item"));
+
+    items[currentItem].scrollIntoView({
+      behavior: "smooth",
+      inline: "center"
+    });
+
+    items[currentItem].classList.add("current-item");
+  });
+});
